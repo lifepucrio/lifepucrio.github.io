d5120e93d535e0ed0e5d78bde1ffd4d29d850f33
lifepucrio <lifepucrio@gmail.com>
2021-05-24 12:12:42 -0300

    1

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var tex = true;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+  if (tex==true) {
+    background(255,0,0);
+  } else {
+    background(0,255,0);
+  }
+}
+
+function touchStarted(){
+  if (tex==true){
+    tex = false;
+  }else {
+    tex = true;
+  }
+
+}
