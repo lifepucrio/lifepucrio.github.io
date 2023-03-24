2b11e40eefa8b8f69877883f3431ec8245d3fb01
lifepucrio <lifepucrio@gmail.com>
2023-03-24 08:54:48 -0300

    a

---
+++ vini/vini.js
@@ -0,0 +1,24 @@
+let input;
+let img;
+
+function setup() {
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+}
+
+function draw() {
+  background(255);
+  if (img) {
+    image(img, 0, 0, width, height);
+  }
+}
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    img.hide();
+  } else {
+    img = null;
+  }
+}
