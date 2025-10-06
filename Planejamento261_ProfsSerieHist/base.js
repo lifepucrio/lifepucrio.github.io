b1932c105a3a9433112b9adb533c0450c57fbc1a
lifepucrio <lifepucrio@gmail.com>
2025-10-06 14:49:06 -0300

    a

---
+++ Planejamento261_ProfsSerieHist/base.js
@@ -0,0 +1,45 @@
+function base() {
+  fill(255, 100);
+  //stroke(255, 100);
+  //strokeWeight(10);
+  noStroke();
+  //rects dias
+  //rect(posSegunda-espaco, pos7h-espaco, largRect, 620, 15);
+  //rect(posTerca-espaco, pos7h-espaco, largRect, 620, 15);
+  //rect(posQuarta-espaco, pos7h-espaco, largRect, 620, 15);
+  //rect(posQuinta-espaco, pos7h-espaco, largRect, 620, 15);
+  //rect(posSexta-espaco, pos7h-espaco, largRect, 620, 15);
+
+  noStroke();
+  fill(0);
+  textStyle(NORMAL);
+  textAlign(CENTER);
+  text('2020.2', pos202, 130);
+  text('2021.1', pos211, 130);
+  text('2021.2', pos212, 130);
+  text('2022.1', pos221, 130);
+  text('2022.2', pos222, 130);
+  text('2023.1', pos231, 130);
+  text('2023.2', pos232, 130);
+  text('2024.1', pos241, 130);
+  text('2024.2', pos242, 130);
+  text('2025.1', pos251, 130);
+  text('2025.2', pos252, 130);
+  text('2026.1', pos261, 130);
+  textAlign(LEFT);
+  for (let cr=0; cr<=20; cr++) {
+    //text(cr+'cr', 50, 760-cr*30);
+  }
+  //text('8h', 50, pos8h);
+  //text('9h', 50, pos9h);
+  //text('10h', 50, pos10h);
+  //text('11h', 50, pos11h);
+  //text('12h', 50, pos12h);
+  //text('13h', 50, pos13h);
+  //text('14h', 50, pos14h);
+  //text('15h', 50, pos15h);
+  //text('16h', 50, pos16h);
+  //text('17h', 50, pos17h);
+  //text('18h', 50, pos18h);
+  //text('19h', 50, pos19h);
+}
