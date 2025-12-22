6801dc32792f60e6f7df5247a84e7aa891101ff6
lifepucrio <lifepucrio@gmail.com>
2025-12-22 11:00:47 -0300

    a

---
+++ Planejamento261_Salas/Planejamento261_Salas.js
@@ -0,0 +1,66 @@
+function preload() {
+  tabela=loadTable('https://docs.google.com/spreadsheets/d/e/2PACX-1vQKABcRBZCtZZLGf9xQJQWEAHhNAm19532fuZNiMoibn2BSaora9noPJSIKATyZSliXNSwSpFLTVM3B/pub?gid=682566435&single=true&output=csv', 'csv', 'header');
+}
+
+function setup() {
+  createCanvas(1200, 960);
+  console.log(tabela.getRowCount()+'linhas');
+  rectMode(CORNER);
+  qs = createSelect();
+  qs.position(300, 60);
+  qs.option('ART1');
+  qs.option('ART2');
+  qs.option('ART3');
+  qs.option('ART4');
+  qs.option('ART5');
+  qs.option('Barraca');
+  qs.option('Casa XXII');
+  qs.option('IAG3');
+  qs.option('IAG4');
+  qs.option('K08');
+  qs.option('K15');
+  qs.option('K17');
+  qs.option('L120');
+  qs.option('L232');
+  qs.option('L260');
+  qs.option('L328');
+  qs.option('L422');
+  qs.option('L532');
+  qs.option('L658');
+  qs.option('LABMET');
+  qs.option('LIFE');
+  qs.option('LAIA');
+  qs.option('LAMP');
+  qs.option('LABMAC');
+  qs.option('LABFisica');
+  qs.option('NEXT');
+  
+  qs.selected('ART1');
+  qs.changed(selecionaSala);
+}
+
+function selecionaSala() {
+  qualSala = qs.value();
+  redraw();
+}
+
+
+function draw() {
+  noLoop();
+  background(220);
+
+  textFont("Helvetica");
+  textStyle(NORMAL);
+  textSize(20);
+  text("Salas de Aula - Semestre 2026.1", 105, 45);
+  textStyle(BOLD);
+  textSize(25);
+  text(qualSala, 105, 80);
+  //base desenho
+  textSize(14);
+  base();
+  salas();
+
+  //professores();
+  //contagemDeCreditos();
+}
